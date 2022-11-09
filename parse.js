9f0a25a05de5811fb31e26c3165a21db2c52873a
Kevin Harrison <62897443+kh-codes@users.noreply.github.com>
2022-11-09 05:15:54 -0500

    Output snapshot data to XLSX

--- parse.js
+++ parse.js
@@ -1,57 +1,78 @@
 const fs = require("fs");
 const { toBech32, fromBech32 } = require('cosmwasm');
 const axios = require("axios");
+const xlsx = require("node-xlsx");
 
-let validAddresses = [];
-let invalidAddresses = [];
+const minimumBalance = 50;
 
-const checkDelegations = async (address, amount) => {
+const checkDelegations = async (address) => {
     try {
         const response = await axios.get(`https://rest.stargaze-1.publicawesome.dev/cosmos/staking/v1beta1/delegations/${address}`);
         if(!response.data || !response.data.delegation_responses) {
             console.error("Error: Failed to fetch delegations for address: " + address);
+            return {
+                eligible: false
+            }
         }
-        if(response.data.delegation_responses.some(
-            (delegationResponse) => (delegationResponse.delegation.shares / (10**6)) > amount)
-        ) return true;
-    } catch (e) {
-        console.error(e.message);
+        const delegatedStarsBalance = response.data.delegation_responses.find((delegationResponse) => (delegationResponse.delegation.shares / (10**6)) >= minimumBalance);
+        if(delegatedStarsBalance) {
+            return {
+                eligible: true,
+                balance: delegatedStarsBalance.delegation.shares / 10**6
+            }
+        }
+    } catch (error) {
+        console.error(error.message);
         console.error("Error: Failed to fetch delegations for address: " + address);
     }
-    return false;
+    return {
+        eligible: false
+    };
+}
+
+const checkStarsBalance = async (address) => {
+    try {
+        const response = await axios.get(`https://rest.stargaze-1.publicawesome.dev/cosmos/bank/v1beta1/balances/${address}`);
+        if(!response.data || !response.data.balances) {
+            console.error("Error: Failed to fetch balance for address: " + address);
+            return {
+                eligible: false
+            }
+        }
+        const starsBalance = response.data.balances.find((balance) => balance.denom === "ustars" && (balance.amount / (10**6) >= minimumBalance));
+        if(starsBalance) {
+            return {
+                eligible: true,
+                balance: starsBalance.amount / 10**6
+            }
+        }
+    } catch (error) {
+        console.error(error.message);
+        console.error("Error: Failed to fetch balance for address: " + address);
+    }
+    return {
+        eligible: false
+    }
 }
 
 const checkBalances = async (snapshot) => {
-    let minBalanceAddresses = [];
-    let zeroBalanceAddresses = [];
+    let eligibleAddresses = [["Address", "Stars Balance", "Delegated Stars Balance"]];
     for(const address of snapshot) {
-        try {
-            const response = await axios.get(`https://rest.stargaze-1.publicawesome.dev/cosmos/bank/v1beta1/balances/${address}`);
-            if(!response.data || !response.data.balances) {
-                console.error("Error: Failed to fetch balance for address: " + address);
-                continue;
-            }
-            if(!response.data.balances.some((balance) => balance.denom === "ustars" && (balance.amount / (10**6)) > 50)) {
-                if(await checkDelegations(address, 50)) {
-                    minBalanceAddresses.push(address);
-                }
-            } else {
-                minBalanceAddresses.push(address);
-            }
-            if(!(response.data.balances.some((balance) => balance.denom === "ustars" && balance.amount >= 0))) {
-                if(!(await checkDelegations(address, 0))) {
-                    zeroBalanceAddresses.push(address);
-                }
-            }
-        } catch (e) {
-            console.error(e.message);
-            console.error("Error: Failed to fetch balance for address: " + address);
-        }
+            
+        const starsBalance = await checkStarsBalance(address);
+        const delegatedStarsBalance = await checkDelegations(address);
+
+        // Skip if the address doesn't meet the minimum balance requirement
+        if(!starsBalance.eligible && !delegatedStarsBalance.eligible) continue;
+
+        // Save balances and address
+        eligibleAddresses.push([
+            address,
+            starsBalance.balance ?? "X",
+            delegatedStarsBalance.balance ?? "X"
+        ]);
     }
-    return {
-        minBalanceAddresses: minBalanceAddresses,
-        zeroBalanceAddresses: zeroBalanceAddresses
-    };
+    return eligibleAddresses;
 }
 
 const toStars = (address) => {
@@ -73,25 +94,8 @@ const toStars = (address) => {
     }
 };
 
-const checkInventory = async (snapshot) => {
-    let qualifiedAddresses = [];
-    for(const address of snapshot) {
-        try {
-            const response = await axios.get(`https://nft-api.stargaze-apis.com/api/v1beta/profile/${address}/nfts`);
-            if(!response.data) {
-                console.error("Error: Failed to fetch inventory for address: " + address);
-                continue;
-            }
-            if(response.data.length > 3) qualifiedAddresses.push(address);
-        } catch (e) {
-            console.error(e);
-            console.error("Error: Failed to fetch inventory for address: " + address);
-        }
-    }
-    return qualifiedAddresses;
-}
-
 const parseAddresses = () => {
+    let validAddresses = [], invalidAddresses = [];
     const oldData = JSON.parse(fs.readFileSync("./data/original.json", { encoding: "utf8" }));
     let addresses = [];
     for(const value of Object.values(oldData)) {
@@ -106,6 +110,22 @@ const parseAddresses = () => {
             validAddresses.push(validAddress);
         }
     }
+    return {
+        invalidAddresses, validAddresses
+    }
+}
+
+const grabStats = async () => {
+    
+    const {validAddresses, invalidAddresses} = parseAddresses();
+    const eligibleAddresses = await checkBalances(validAddresses);
+
+    // Create buffer for XLSX file including all eligible addresses & balances
+    const spreadsheetBuffer = xlsx.build([{ name: `Parsed Submissions`, data: eligibleAddresses }]);
+    
+    // Write spreadsheet to filesystem
+    fs.writeFileSync("./data/snapshot.xlsx", spreadsheetBuffer);
+    // Write invalid addresses to filesystem
     fs.writeFileSync(
         "./data/invalid_addresses.json",
         JSON.stringify(invalidAddresses, null, 2),
@@ -113,6 +133,7 @@ const parseAddresses = () => {
             encoding: "utf8"
         }
     );
+    // Write valid addresses to filesystem
     fs.writeFileSync(
         "./data/valid_addresses.json",
         JSON.stringify(validAddresses, null, 2),
@@ -120,21 +141,19 @@ const parseAddresses = () => {
             encoding: "utf8"
         }
     );
-}
-
-const grabStats = async () => {
-    parseAddresses();
-    const {minBalanceAddresses, zeroBalanceAddresses} = await checkBalances(validAddresses);
-    const minNFTs = await checkInventory(validAddresses);
-    fs.writeFileSync("./data/50orMoreStars.json", JSON.stringify(minBalanceAddresses, null, 2), { encoding: "utf8" });
-    fs.writeFileSync("./data/zeroBalance.json", JSON.stringify(zeroBalanceAddresses, null, 2), { encoding: "utf8" });
-    fs.writeFileSync("./data/3orMoreNFTs.json", JSON.stringify(minNFTs, null, 2), { encoding: "utf8" });
+    // Write eligible addresses & balances to filesystem
+    fs.writeFileSync(
+        "./data/eligibleAddresses.json", 
+        JSON.stringify(eligibleAddresses, null, 2), 
+        { 
+            encoding: "utf8" 
+        }
+    );
+    
     console.log("### DETAILS ###");
-    console.log(`Valid addresses: ${validAddresses.length}`);
-    console.log(`Invalid addresses: ${invalidAddresses.length}`);
-    console.log("Addresses with >50 $stars (delegated or undelegated): " + minBalanceAddresses.length);
-    console.log("Addresses with 0 $stars (delegated or undelegated): " + zeroBalanceAddresses.length);
-    console.log("Addresses with > 3 NFTs: " + minNFTs.length);
+    console.log("Valid addresses: " + validAddresses.length);
+    console.log("Invalid addresses: " + invalidAddresses.length);
+    console.log("Eligible addresses (>50 $stars delegated or > 50 $stars undelegated): " + eligibleAddresses.length);
 }
 
 grabStats();
